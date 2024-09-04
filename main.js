c954dc889bb20cbeb030aa3df05391430346af74
zxlee0114 <napoleon.lee0114@gmail.com>
2024-09-04 18:50:24 +0800

    0904 修改

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ import './assets/scss/all.scss';
 
 AOS.init({
     easing: 'ease-out-back',
-    duration: 800
+    duration: 500
 });
 
-hljs.initHighlightingOnLoad();
 
-			$('.hero__scroll').on('click', function(e) {
-				$('html, body').animate({
-					scrollTop: $(window).height()
-				}, 1200);
-			});
+// hljs.initHighlightingOnLoad();
+
+// 			$('.hero__scroll').on('click', function(e) {
+// 				$('html, body').animate({
+// 					scrollTop: $(window).height()
+// 				}, 1200);
+// 			});
 		
 console.log("Hello world!");
